5160432ad631c7882835d066206f587cec085147
yuyuyu <yuyuyu@example.com>
2022-01-30 04:49:50 +0000

    Update grouplist.py

--- grouplist.py
+++ grouplist.py
@@ -7,5 +7,5 @@ KEY="4b65396a022e1658495caeef7e0684a8dcd186a7"
 redmine = Redmine(URL, key=KEY)
 groups = redmine.group.all(sort='category:desc')
 for group in groups:
-  print ('{},{},{}'.format(group.id, group.name, group.memberships.values))
+  print ('{},{}'.format(group.id, group.name))
 
