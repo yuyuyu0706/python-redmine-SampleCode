8c406e69be5336268e193506fe8397de97631ecd
yuyuyu <yuyuyu@example.com>
2022-01-30 05:03:33 +0000

    Update userlist.py last login

--- userlist.py
+++ userlist.py
@@ -7,5 +7,6 @@ KEY="4b65396a022e1658495caeef7e0684a8dcd186a7"
 redmine = Redmine(URL, key=KEY)
 users = redmine.user.all(sort='category:desc')
 for user in users:
-  print ('{},{}'.format(user.id, user.firstname))
+  last_login_on = getattr(user, "last_login_on", None)
+  print ('{},{},{}'.format(user.id, user.firstname, last_login_on))
 
