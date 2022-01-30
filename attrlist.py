831609ffb65bf42bcc57c7f50091d26f58e951d5
yuyuyu <yuyuyu@example.com>
2022-01-30 13:27:40 +0000

    Update attrlist.py add type print

--- attrlist.py
+++ attrlist.py
@@ -12,21 +12,30 @@ group = redmine.group.get(6)
 issue = redmine.issue.get(1)
 field = redmine.custom_field.get(1)
 
+print("Redmine AttrList")
+print("----------------")
+print("{}\n".format(type(redmine)))
+
 print("Project AttrList")
 print("----------------")
+print("{}\n".format(type(project)))
 print("{}\n".format(dir(project)))
 
 print("User AttrList")
 print("----------------")
+print("{}\n".format(type(user)))
 print("{}\n".format(dir(user)))
 
 print("Group AttrList")
 print("----------------")
+print("{}\n".format(type(group)))
 print("{}\n".format(dir(group)))
 
 print("Group users AttrList")
 print("----------------")
 if len(group.users) > 0:
+  print("{}".format(type(group.users)))
+  print("{}\n".format(type(list(group.users)[0])))
   print("{}\n".format(dir(list(group.users)[0])))
 else:
   print("Empty Group users\n")
@@ -34,15 +43,18 @@ else:
 print("Group Memberships AttrList")
 print("----------------")
 if len(group.memberships) > 0:
+  print("{}\n".format(type(list(group.memberships)[0])))
   print("{}\n".format(dir(list(group.memberships)[0])))
 else:
   print("Empty Memberships\n")
 
 print("Issue AttrList")
 print("----------------")
+print("{}\n".format(type(issue)))
 print("{}\n".format(dir(issue)))
 
 print("Custom Field AttrList")
 print("----------------")
+print("{}\n".format(type(field)))
 print("{}\n".format(dir(field)))
 
